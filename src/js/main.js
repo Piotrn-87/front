525a00460f2d086b5d16b26a9eaac61b2c374e4f
Piotr Napieralski <piotrn-87@o2.pl>
2019-07-04 19:32:20 +0200

    Feature: Adding main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,41 @@
-"use strict";
+const headerText = document.querySelector(".hero__header--text");
+const burgerMenu = document.querySelector(".burger");
+const mobileNav = document.querySelector(".mobile-nav");
+const activeElement = document.querySelectorAll(".active");
+const backdrop = document.querySelector(".backdrop");
+const closeModal = document.querySelector(".mobile__nav--items");
 
-import moment from 'moment';
+const text = "Frontend Resources !!"
+const time = 200;
+let index = 0;
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+const addLetter = () => {
+    headerText.textContent += text[index];
+    index++;
+    if (index === text.length) {
+        clearInterval(headerTyping);
+    }
+}
+const headerTyping = setInterval(addLetter, time);
+
+burgerMenu.addEventListener("click", (e) => {
+    for (let i = 0; i < activeElement.length; i++) {
+        activeElement[i].classList.toggle("show_icon");
+        activeElement[i].classList.toggle("mobile-nav__visible");
+        activeElement[i].classList.toggle("backdrop-active");
+    }
+})
+
+closeModal.addEventListener("click", (e) => {
+    mobileNav.classList.remove("mobile-nav__visible");
+    backdrop.classList.remove("backdrop-active");
+    for (let i = 0; i < activeElement.length; i++) {
+        activeElement[i].classList.toggle("show_icon");
+    }
+})
+
+// backdrop.addEventListener("click", (e) => {
+//     mobileNav.classList.remove("mobile-nav__visible");
+//     backdrop.classList.remove("backdrop-active");
+//     console.log("dziala");
+// })
